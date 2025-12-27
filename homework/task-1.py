78560e07e6fe8a388ca26491ab43d9a5102a6621
dimooon835 <dimooon835@gmail.com>
2025-12-27 20:48:57 +0300

    Update task-1.py

--- homework/task-1.py
+++ homework/task-1.py
@@ -1,9 +1,17 @@
-num1 = int(input("Введите число 1: "))
-num2 = int(input("Введите число 2: "))
-num3 = int(input("Введите число 3: "))
+# num1 = int(input("Введите число 1: "))
+# num2 = int(input("Введите число 2: "))
+# num3 = int(input("Введите число 3: "))
 
-sum = num1 + num2 + num3
-multip = num1 * num2 * num3
+# sum = num1 + num2 + num3
+# multip = num1 * num2 * num3
 
-print(f"Сумма: {sum}")
-print(f"Произведение: {multip}")
+# print(f"Сумма: {sum}")
+# print(f"Произведение: {multip}")
+
+
+num1 = int(input("Зарплата за месяц: "))
+num2 = int(input("Сумма месячного платежа по кредиту: "))
+num3 = int(input("Задолженность за коммунальные услуги: "))
+
+sum = num1 - num2 - num3
+print(f"Остаток после выплат: ")
