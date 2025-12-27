2fc00da52947760994c69a91c2be30bd2a946e06
dimooon835 <dimooon835@gmail.com>
2025-12-27 18:28:27 +0300

    Delete task-1.py

--- classwork/task-1.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Введите число 1: "))
-num2 = int(input("Введите число 2: "))
-num3 = int(input("Введите число 3: "))
-
-sum = num1 + num2 + num3
-multip = num1 * num2 * num3
-
-print(f"Сумма: {sum}")
-print(f"Произведение: {multip}")
