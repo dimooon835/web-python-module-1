7f0d43261468ac273f040af2fe52f93b4a67f3be
dimooon835 <dimooon835@gmail.com>
2026-01-21 20:47:34 +0300

    feat:Выполнение задания по модулю 3

--- module-2/classwork/task_6.py
+++ module-2/classwork/task_6.py
@@ -42,12 +42,109 @@
 # print(result)
 
 
+# # 1
+# text = input("Введите текст: ")
+
+# def process_text(text):
+#     update_text = ". ".join(s.capitalize() for s in text.split("."))
+
+#     digits_count = 0
+#     punc_count = 0
+#     excl_count = 0
+
+#     for char in update_text:
+#         if char.isdigit():
+#             digits_count += 1
+#         elif char in ".,!?:;":
+#             punc_count += 1
+#         elif char == "!":
+#             excl_count += 1
+
+#     return update_text, digits_count, punc_count, excl_count
+
+# update_text, digits_count, punc_count, excl_count = process_text(text)
+
+# print(update_text)
+# print(f"Кол-во цифр {digits_count}")
+# print(f"Кол-во знаков препинания {punc_count}")
+# print(f"Кол-во восклицательных знаков {excl_count}")
+
+# # 2
+# num = input("Введите числа: ")
+# search = input("Введите число для поиска: ")
+
+# count = num.count(search)
+# print(f"Число {search} встречается в списке {count} раз")
+
+# # 3
+# numbers = (input("Введите числа через пробел: "))
+# num = numbers.split()
+
+# for i in range(len(num)):
+#     num[i] = int(num[i])
+
+# total = sum(num)
+# average = total / len(num)
+
+# print(f"Сумма чисел: {total}")
+# print(f"Средняя чисел: {average}")
+
+
 # 1
-text = input("Введите текст: ")
+text = input("Введите числа: ")
+numbers = text.split()
 
-update_text = ". ".join(s.capitalize() for s in text.split("."))
+for i in range(len(numbers)):
+    numbers[i] = int(numbers[i])
 
-print(update_text)
+def process_numbers(numbers):
+    sum_negative = 0
+    sum_even = 0
+    sum_odd = 0
+    index_3 = 1
+    min_index = max_index = 0
 
+    for num in numbers:
+        if num < 0:
+            sum_negative += num
+        if num % 2 == 0:
+            sum_even += num
+        else: 
+            sum_odd += num
 
-# 2
+    for i in range(0, len(numbers), 3):
+        index_3 *= numbers[i]
+
+    start = min(min_index, max_index)
+    end = max(min_index, max_index)
+    index = 1
+    for i in range(start, end):
+        index *= numbers[i]
+
+    first_positive = -1
+    last_positive = -1
+    for i, num in enumerate(numbers):
+        if num > 0:
+            if first_positive == -1:
+                first_positive = i
+            last_positive = i
+        
+    if first_positive != -1 and last_positive != first_positive:
+        sum_between = sum(numbers[first_positive+1:last_positive])
+    else:
+        sum_between = 0
+
+    return sum_negative, sum_even, sum_odd, index_3, index, sum_between
+
+# result = process_numbers(numbers)
+# for key, value in result():
+#     print(f"{key}: {value}")
+
+sum_negative, sum_even, sum_odd, index_3, index, sum_between = process_numbers(numbers)
+
+print(sum_negative)
+print(sum_even)
+print(sum_odd)
+print(index_3)
+print(index)
+print(sum_between)
