add51228490c1cc9259bdc07c8253514151b07b8
dimooon835 <dimooon835@gmail.com>
2026-02-02 20:48:35 +0300

    Create task_4.py

---
+++ module-3/task_4.py
@@ -0,0 +1,31 @@
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n - 1):
+        swapped = False
+
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+    return arr
+print(bubble_sort([5, 2, -5, 1, 0]))
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+    return arr
+print(insertion_sort([10, 0, -2, 9, 3]))
