75ceb8cfc3c984ff734318256767224bf440eb2d
dimooon835 <dimooon835@gmail.com>
2026-04-08 20:41:20 +0300

    Теория по модулю 12 и 13, а также решение задач по модулю 12 от 08.04.26

--- module-12/theory.py
+++ module-12/theory.py
@@ -490,3 +490,59 @@ class DeliveryCalc:
         return self.strategy.calculate(weight)
     
 print(DeliveryCalc(PickupDelivery()).get_price(10))
+
+
+# Поведенческие паттерны
+# Template Method
+
+class ReportTemplate:
+    def build(self):
+        data = self.fetch_data()
+        return self.format_data(data)
+    
+    def fetch_data(self):
+        raise NotImplementedError
+    
+    def format_data(self, data):
+        raise NotImplementedError
+    
+class SalesReport(ReportTemplate):
+    def fetch_data(self):
+        return [100, 200, 300]
+    
+    def format_data(self, data):
+        return f"Сумма продаж: {sum(data)}"
+    
+print(SalesReport().build())
+
+
+# Поведенческие паттерны
+# Visitor
+
+class Book:
+    def __init__(self, title, price):
+        self.title = title
+        self.price = price
+
+    def accept(self, visitor):
+        return visitor.visit_book(self)
+    
+class Course:
+    def __init__(self, title, price):
+        self.title = title
+        self.price = price
+
+    def accept(self, visitor):
+        return visitor.visit_book(self)
+    
+class DiscountVisitor:
+    def visit_book(self, book):
+        return f"{book.title}: {book.price * 0.9}"
+    
+    def visit_course(self, course):
+        return f"{course.title}: {course.price * 0.2}"
+    
+visitor = DiscountVisitor
+items = [Book("book", 1000), Course("course", 5000)]
+for item in items:
+    print(item.accept(visitor))
---
+++ module-13/theory.py
@@ -0,0 +1,58 @@
+# Model
+
+from dataclasses import dataclass
+
+@dataclass
+
+class Product:
+    name: str
+    price: int
+
+class CartModel:
+    def __init__(self):
+        self.products = {
+            "apple": Product("Яблоко", 80),
+            "banana": Product("Банан", 60),
+            "coffee": Product("Кофе", 250)
+        }
+
+        self.items = []
+
+    def add_item(self, product_code: str):
+        if product_code not in self.products:
+            raise ValueError("Такого товара нет в каталоге")
+        self.items.append(self.products(product_code))
+
+    def total(self):
+        return sum(item.price for item in self.items)
+    
+    def item_names(self):
+        return [item.name for item in self.items]
+    
+model = CartModel()
+model.add_item("apple")
+model.add_item("coffee")
+print(model.item_names())
+print(model.total)
+
+
+# View
+
+class ConsoleCartView:
+    @staticmethod
+    def render_cart(items, total):
+        print("Корзина:")
+
+        if items:
+            for item in items:
+                print(f"- {item}")
+        else:
+            print("- пусто")
+        print(f"Итого: {total}")
+
+    @staticmethod
+    def render_error(msg):
+        print("Ошибка: {msg}")
+
+ConsoleCartView.render_cart(["Яблоко", "Кофе"], 330)
+
